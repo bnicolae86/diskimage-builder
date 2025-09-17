7b72fa2c85ce7459573a3241ceb70c7da0f7dcd1
bnicolae86 <54812815+bnicolae86@users.noreply.github.com>
2025-09-17 10:24:24 +0300

    add support for SLES 15 SP7

--- diskimage_builder/elements/suse-linux-enterprise/environment.d/10-sle-distro-name.bash
+++ diskimage_builder/elements/suse-linux-enterprise/environment.d/10-sle-distro-name.bash
@@ -5,8 +5,8 @@ export DIB_INIT_SYSTEM=systemd
 case ${DIB_RELEASE} in
     15.5) export SLE_REPO_DIR=SLE_${DIB_RELEASE} ;;
     15.6) export SLE_REPO_DIR=SLE_${DIB_RELEASE} ;;
+    15.7) export SLE_REPO_DIR=SLE_${DIB_RELEASE} ;;
     *) echo "Unsupported SLE release: ${DIB_RELEASE}"; exit 1 ;;
 esac
 
 export DIB_DISTRIBUTION_MIRROR=${DIB_DISTRIBUTION_MIRROR:-https://download.suse.com}
-
